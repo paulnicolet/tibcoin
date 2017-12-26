04257a6b05e84530c44dbb805c031beff25323d7
paulnicolet <nicoletpaul@orange.fr>
2017-12-26 10:42:11 +0100

    Implement hash and equals methods

--- common/blockchain.go
+++ common/blockchain.go
@@ -12,3 +12,7 @@ type Sig struct {
 type Address struct {
 	PubKeyHash []byte
 }
+
+func (sig Sig) Equal(other Sig) bool {
+	return sig.R.Cmp(other.R) == 0 && sig.S.Cmp(other.S) == 0
+}
--- gossipernode/tx_helper.go
+++ gossipernode/tx_helper.go
@@ -1,15 +1,72 @@
 package gossipernode
 
+import (
+	"bytes"
+	"crypto/sha256"
+	"strconv"
+)
+
 func (tx *Transaction) getSignable() []byte {
-	// Return signable version of transaction
-	// This should exclude tx signature and hash the result
-	return nil
+	h := sha256.New()
+
+	// Hash inputs
+	for _, input := range tx.inputs {
+		h.Write(input.hash())
+	}
+
+	// Hash ouputs
+	for _, output := range tx.outputs {
+		h.Write(output.hash())
+	}
+
+	return h.Sum(nil)
 }
 
 func (tx *Transaction) equals(other *Transaction) bool {
-	return false
+	if !tx.sig.Equal(other.sig) {
+		return false
+	}
+
+	for i, input := range tx.inputs {
+		if !input.equals(&other.inputs[i]) {
+			return false
+		}
+	}
+
+	for i, output := range tx.outputs {
+		if !output.equals(&other.outputs[i]) {
+			return false
+		}
+	}
+
+	return true
+}
+
+// Transaction inputs internals
+func (in *TxInput) hash() []byte {
+	h := sha256.New()
+	h.Write(in.outputTxHash)
+	h.Write([]byte(strconv.Itoa(int(in.outputIdx))))
+	return h.Sum(nil)
+}
+
+func (in *TxInput) equals(other *TxInput) bool {
+	return bytes.Equal(in.outputTxHash, other.outputTxHash) && in.outputIdx == other.outputIdx
 }
 
 func (in *TxInput) getOutput() (*TxOutput, error) {
 	return nil, nil
+}
+
+// Transaction outputs internals
+
+func (out *TxOutput) hash() []byte {
+	h := sha256.New()
+	h.Write(out.to.PubKeyHash)
+	h.Write([]byte(strconv.Itoa(int(out.value))))
+	return h.Sum(nil)
+}
+
+func (out *TxOutput) equals(other *TxOutput) bool {
+	return bytes.Equal(out.to.PubKeyHash, other.to.PubKeyHash) && out.value == other.value
 }
